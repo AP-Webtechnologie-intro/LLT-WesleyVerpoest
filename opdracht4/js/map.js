467e747f9999fdcaba2f8abb2b387cfe1c9410e4
WesleyVerpoest <s152161@ap.be>
2023-12-18 16:36:51 +0100

    opdracht 4 aangemaakt
     overgezet van map opdracht 3 naar opdracht 4
    mappen toegevoeg op pagina contact.html en bierproeverij.html
    marker aangepast naar bier icoon
    api gekoppeld aan carousel op pagina bier_en_gerechten.html
    javascriptvoor mappen en api gemaakt
    css voor mappen gemaakt

---
+++ opdracht4/js/map.js
@@ -0,0 +1,21 @@
+
+let map = L.map('contact-Map').setView([51.24296118765395, 4.438330635754], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let bierIcon = L.icon({
+    iconUrl: './images/bier-icon.png',
+
+    iconSize:     [40 ,40], // size of the icon
+    
+    iconAnchor:   [30, 30], // point of the icon which will correspond to marker's location
+    
+    popupAnchor:  [-10, -30] // point from which the popup should open relative to the iconAnchor
+});
+
+
+let marker = L.marker([51.24296118765395, 4.438330635754],{icon: bierIcon}).addTo(map);
+marker.bindPopup("<strong>Bierwereld</strong>").openPopup()
